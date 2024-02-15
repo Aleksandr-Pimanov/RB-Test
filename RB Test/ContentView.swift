fe337ab51d61bfaf3d78deb243fc1c0d55f32987
Aleksandr Pimanov <aleksandr.pimanov@al-enterprise.com>
2024-02-15 14:13:56 +0300

    Initial Commit

---
+++ RB Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RB Test
+//
+//  Created by Александр Пиманов on 15.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RB Test/RB_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RB_TestApp.swift
+//  RB Test
+//
+//  Created by Александр Пиманов on 15.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct RB_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
